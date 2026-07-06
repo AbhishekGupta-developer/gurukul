88f9bedd7f53d8b51a2498ece22bd07dcbdd8dc7
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-07-06 20:55:25 +0530

    Add custom JPQL and native SQL query methods

--- src/main/java/com/myorganisation/gurukul/repository/StudentRepository.java
+++ src/main/java/com/myorganisation/gurukul/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.myorganisation.gurukul.repository;
 
 import com.myorganisation.gurukul.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +15,16 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByCourse(String course);
     List<Student> findByCourseContaining(String course);
     List<Student> findByNameContainingAndCourseContaining(String name, String course);
+
+    // JPQL -> Java Persistence Query Language
+    @Query("SELECT s FROM Student s WHERE s.course = :c")
+    List<Student> searchStudentsByCourse(@Param("c") String course);
+
+    @Query("SELECT s FROM Student s WHERE s.course LIKE %:c%")
+    List<Student> searchStudentsByCourseLike(@Param("c") String course);
+
+    // Native SQL
+    @Query(value = "SELECT s.* FROM students s WHERE s.mail LIKE %:email%", nativeQuery = true)
+    List<Student> searchStudentsByEmailContaining(@Param("email") String email);
 
 }
