68763a865d31db36446df032eab2ce9210b29577
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-07-13 20:43:10 +0530

    Add vehicle in response dto

--- src/main/java/com/myorganisation/gurukul/dto/response/StudentResponseDto.java
+++ src/main/java/com/myorganisation/gurukul/dto/response/StudentResponseDto.java
@@ -1,5 +1,6 @@
 package com.myorganisation.gurukul.dto.response;
 
+import com.myorganisation.gurukul.entity.Vehicle;
 import com.myorganisation.gurukul.enums.Gender;
 import lombok.Data;
 
@@ -11,4 +12,5 @@ public class StudentResponseDto {
     private String phone;
     private String email;
     private Gender gender;
+    private Vehicle vehicle;
 }
